71b8464d4f52dacc734b957c79b497e7a5e19fd2
IvayloTashev <i.tashev@abv.bg>
2024-08-19 23:05:21 +0300

    Initializing the project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import React from "react";
+
+const App = () => (
+    <BrowserRouter>
+
+    <Box sx={{ backgroundColor: '#000'}}>
+    Navbar
+
+    <Routes>
+
+
+    </Routes>
+    </Box>
+
+    </BrowserRouter>
+);
+
+export default App
