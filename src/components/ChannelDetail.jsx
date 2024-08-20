54703b502318f4bc37338b857e77243ae43c7c24
IvayloTashev <i.tashev@abv.bg>
2024-08-20 19:04:57 +0300

    Add routing and with empty components

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,5 @@
+export default function ChannelDetail() {
+   return (
+       <>Channel Details</>
+   );
+}
---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,5 @@
+export default function SearchFeed() {
+   return (
+       <>Search Feed</>
+   );
+}
