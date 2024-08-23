062f9d78d589444969e3c324660d4051b5e62bff
IvayloTashev <i.tashev@abv.bg>
2024-08-23 19:42:00 +0300

    Update feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,14 +2,29 @@ import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import { Sidebar, Videos } from "./";
 
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
 
 export default function Feed() {
+
+    const [selectedCategory, setSelectedCategory] = useState(`New`);
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const data = await fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+            setVideos(data.items);
+        })();
+    }, [selectedCategory]);
+
     return (
         <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
             <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-                <Sidebar>
+                <Sidebar
+                    selectedCategory={selectedCategory}
+                    setSelectedCategory={setSelectedCategory}
+                />
 
-                </Sidebar>
 
                 <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
                     Copyright 2024
@@ -23,12 +38,12 @@ export default function Feed() {
                     mb={2}
                     sx={{ color: 'white' }}
                 >
-                    New <span style={{ color: '#F31503' }}>
+                    {selectedCategory} <span style={{ color: '#F31503' }}>
                         Videos
                     </span>
                 </Typography>
 
-                <Videos videos={[]} />
+                <Videos videos={videos} />
 
             </Box>
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
 
-
-const selectedCategory = 'New';
-
-export default function Sidebar() {
+export default function Sidebar({
+    selectedCategory,
+    setSelectedCategory
+}) {
     return (
         <Stack
             direction='row'
@@ -17,6 +17,7 @@ export default function Sidebar() {
             {categories.map(category => (
                 <button
                     className="category-btn"
+                    onClick={() => setSelectedCategory(category.name)}
                     style={{
                         background: category.name === selectedCategory && '#FC1503',
                         color: 'white'
