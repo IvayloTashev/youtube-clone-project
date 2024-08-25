318b40f5a805ca40c70f98ef2a6fdffdcca7c5e6
IvayloTashev <i.tashev@abv.bg>
2024-08-25 11:36:37 +0300

    Add ChannelCard component

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,5 +1,49 @@
-export default function ChannelCard() {
+import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/constants";
+
+
+export default function ChannelCard({
+    channelDetail
+}) {
+
     return (
-        <>ChannelCard</>
+        <Box
+            sx={{
+                boxShadow: 'none',
+                borderRadius: '20px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: { xs: '356px', md: '320 px' },
+                height: '326px',
+                margin: 'auto'
+            }}
+        >
+            <Link to={`/channel${channelDetail?.id?.channelId}`}>
+                <CardContent
+                    sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'conter', textAlign: 'center', color: '#fff' }}
+                >
+                    <CardMedia
+                        image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+                        alt={channelDetail?.snippet?.title}
+                        sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
+                    />
+                    <Typography variant="h6">
+                        {channelDetail?.snippet?.title}
+                        <CheckCircle
+                            sx={{ fontSize: 14, color: 'grey', ml: '5px' }}
+                        />
+                    </Typography>
+                    {channelDetail?.statistics?.subscriberCount && (
+                        <Typography>
+                            {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+                        </Typography>
+                    )}
+                </CardContent>
+            </Link>
+
+        </Box>
     );
 }
