159ccbed5e19d3ea806d1935dfa1f53e41dce64b
IvayloTashev <i.tashev@abv.bg>
2024-08-27 19:06:36 +0300

    Add SearchFeed component

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,5 +1,35 @@
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { Videos } from "./";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+
 export default function SearchFeed() {
-   return (
-       <>Search Feed</>
-   );
+    const [videos, setVideos] = useState([]);
+    const { searchTerm } = useParams();
+
+    useEffect(() => {
+        (async () => {
+            const data = await fetchFromAPI(`search?part=snippet&q=${searchTerm}`);
+            setVideos(data.items);
+        })();
+    }, [searchTerm]);
+
+    return (
+        <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+            <Typography
+                variant="h4"
+                fontWeight='bold'
+                mb={2}
+                sx={{ color: 'white' }}
+            >
+                Search results for: <span style={{ color: '#F31503' }}>{searchTerm} videos</span>
+            </Typography>
+
+            <Videos videos={videos} />
+
+        </Box>
+    );
 }
